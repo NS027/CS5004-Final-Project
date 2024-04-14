41214836ac94763d07fce09f162231e9ce5c6ae2
NS027 <chris_n89@hotmail.com>
2024-04-14 12:03:42 -0700

    add request and random request to controller

--- src/controller/ElevatorControllerImpl.java
+++ src/controller/ElevatorControllerImpl.java
@@ -2,11 +2,13 @@ package controller;
 
 
 import building.Building;
-
+import java.util.Random;
+import scanerzus.Request;
 
 
 public class ElevatorControllerImpl {
   private Building building;
+  private Random random;
 
   /**
    * Constructor for the ElevatorControllerImpl class
@@ -17,6 +19,7 @@ public class ElevatorControllerImpl {
    */
   public ElevatorControllerImpl(int numFloors, int numElevators, int capacity) {
     building = new Building(numFloors, numElevators, capacity);
+    random = new Random();
   }
 
   /**
@@ -47,6 +50,45 @@ public class ElevatorControllerImpl {
     for (int i = 0; i < n; i++) {
       building.stepElevatorSystem();
     }
+  }
+
+  /**
+   * This is method used to add request to the elevator controller
+   */
+  public void addRequest(int fromFloor, int toFloor) {
+    building.addRequestToElevatorSystem(new Request(fromFloor, toFloor));
+  }
+
+  /**
+   * This is method used to add n request to the elevator controller
+   */
+  public void addRandomRequest(int n) {
+    // for evenly distributed requests
+    int half = n / 2;
+
+    // for the up requests
+    for (int i = 0; i < half; i++) {
+      generateRequest(true);
+    }
+    // for the down requests
+    for (int i = 0; i < n - half; i++) {
+      generateRequest(false);
+    }
+  }
+
+  /**
+   * This is method used to generate random request
+   */
+  private void generateRequest(boolean isUp) {
+    int startFloor, endFloor;
+    if (isUp) {
+      startFloor = random.nextInt(building.getNumberOfFloors() - 1);
+      endFloor = random.nextInt(building.getNumberOfFloors() - startFloor - 1) + startFloor + 1;
+    } else {
+      endFloor = random.nextInt(building.getNumberOfFloors() - 1);
+      startFloor = random.nextInt(building.getNumberOfFloors() - endFloor - 1) + endFloor + 1;
+    }
+    building.addRequestToElevatorSystem(new Request(startFloor, endFloor));
   }
 
 }
