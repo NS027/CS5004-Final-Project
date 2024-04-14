60d6ae629a6bb232ac023bc27e44611e45701355
NS027 <chris_n89@hotmail.com>
2024-04-14 11:44:54 -0700

    add stop and start in controller

--- src/controller/ElevatorControllerImpl.java
+++ src/controller/ElevatorControllerImpl.java
@@ -10,9 +10,26 @@ public class ElevatorControllerImpl {
 
   /**
    * Constructor for the ElevatorControllerImpl class
+   *
+   * @param numFloors the number of floors in the building
+   * @param numElevators the number of elevators in the building
+   * @param capacity the capacity of each elevator
    */
   public ElevatorControllerImpl(int numFloors, int numElevators, int capacity) {
     building = new Building(numFloors, numElevators, capacity);
   }
+
+  /**
+   * This is method used to start the elevator controller
+   */
+  public void startSystem() {
+    building.startElevatorSystem();
+  }
+
+  /**
+   * This is method used to stop the elevator controller
+   */
+  public void stopSystem() {
+    building.stopElevatorSystem();
 
 }
