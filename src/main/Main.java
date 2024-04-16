fef7c6758035d7abe834f9b43792d9de674a26a2
NS027 <chris_n89@hotmail.com>
2024-04-16 02:39:18 -0700

    modify the main

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,10 +6,27 @@ import javax.swing.SwingUtilities;
 
 public class Main {
   public static void main(String[] args) {
-    // Parameters for the elevator system
-    final int numFloors = 10;
-    final int numElevators = 4;
-    final int capacity = 3;
+    // Default values in case arguments are not provided or are invalid
+    int numFloors = 10;
+    int numElevators = 2;
+    int capacity = 3;
+
+    // Check if the right number of arguments are passed
+    if (args.length >= 3) {
+      try {
+        numFloors = Integer.parseInt(args[0]);
+        numElevators = Integer.parseInt(args[1]);
+        capacity = Integer.parseInt(args[2]);
+      } catch (NumberFormatException e) {
+        System.err.println("Invalid arguments. Please ensure all arguments are integers.");
+        System.exit(1);  // Exit the program with an error code
+      }
+    } else {
+      System.out.println("Not enough arguments provided. Using default values:");
+      System.out.println("Number of Floors: " + numFloors);
+      System.out.println("Number of Elevators: " + numElevators);
+      System.out.println("Capacity per Elevator: " + capacity);
+    }
 
     // Create the controller with the building parameters
     ElevatorControllerImpl controller = new ElevatorControllerImpl(numFloors, numElevators, capacity);
@@ -18,5 +35,3 @@ public class Main {
     SwingUtilities.invokeLater(() -> new ElevatorSystemView(controller));
   }
 }
-
-
